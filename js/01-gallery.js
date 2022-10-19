1eafa7027fa3f7993828bc8b25293c06b87783eb
Vadym <besarabvadim2@gmail.com>
2022-10-19 11:59:35 +0300

    mdsnbvjhrefgj

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const cardsMarkup = createMarkup(galleryItems);
+galleryRef.addEventListener("click", onClickEvent);
 
-console.log(galleryItems);
+function onClickEvent(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="600" height="600">`);
+
+    if(!event.target.classList.contains("gallery__image")) {
+        return;
+   }
+    instance.show();
+}
+
+
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return`
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src= "${preview}"
+      data-source= "${original}"
+      alt= "${description}"
+    />
+  </a>
+    </div>`;
+    }).join(" ");
+
+};
+console.log(cardsMarkup);
+galleryRef.insertAdjacentHTML("beforeend", cardsMarkup);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const cardsMarkup = createMarkup(galleryItems);
+
+
+function onClickEvent(event) {
+    event.preventDefault();
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+    captionData: "alt",
+    captionDelay: 250,
+    });
+    
+    if(event.target.nodeName !== "IMG") {
+        return
+   }
+    lightbox.open();
+}
+
+galleryRef.addEventListener("click", onClickEvent);
+    function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return`
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;}).join(" ");
+};
+
+console.log(cardsMarkup);
+galleryRef.insertAdjacentHTML("beforeend", cardsMarkup);
 
 console.log(galleryItems);
